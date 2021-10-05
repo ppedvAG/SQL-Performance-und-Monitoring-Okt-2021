d87d5c507b76e859b5854a66379a58f8992f555a
Rauchendraser <andreasr@ppedv.de>
2021-10-05 14:33:16 +0200

    Virtualisierung, Setup

---
+++ SQL PerfMonitor/SQL PerfMonitor/01_VM.sql
@@ -0,0 +1,44 @@
+--Virtualisierung
+
+--BWIN: in der Sekunde 100000
+--AMAZON: am Tag in DEU 12 Mio TX
+
+--Aufpassen:
+
+/*
+praktisch
+Konsoldierung 5 SQL Server virt auf einem Server
+
+auch die HDDs?  
+SAN?
+X: 100000 IOPS Laufwerk shared
+Y: 70000 IOPS dediziert
+
+regelm messen--> unter Windows Perfmon
+
+Taskmanager
+1 Socket  4 Prozessoren
+8GB
+
+für VM
+dynamischer Arbeitsspeicher: schlecht
+fest zuweisen...!!
+
+SQL OS = Betriebssystem
+
+20GB  8GB -- 16GB
+
+
+Prozessoren
+
+NUMA
+jeder Socket hat seie eig Speicherslots
+eine Kommunikation wischen den Sockets ist aufwendig
+
+Start: 10GB
+
+
+
+
+
+*/
---
+++ SQL PerfMonitor/SQL PerfMonitor/02_Setup.sql
@@ -0,0 +1,30 @@
+--Setup:
+
+/*
+Pfade für DBs
+--Daten und Logs
+
+ab sql 2016 im Setup:
+
+tempdb
+
+
+RAM 
+Min
+Max
+
+
+MAXDOP
+
+Datenträgervolumewartungstask
+
+
+
+Security
+
+
+
+
+
+
+*/
